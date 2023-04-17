eeab503675445b585dcb20b32fde1e9034366828
hldh214 <hldh214@gmail.com>
2023-04-17 15:36:37 +0900

    fix(config): fix config loading for `pyinstaller`
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- buff2steam/__init__.py
+++ buff2steam/__init__.py
@@ -5,7 +5,15 @@ import sys
 
 import loguru
 
-base_path = pathlib.Path(os.path.abspath(__file__)).parent.parent
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    # https://pyinstaller.org/en/stable/runtime-information.html
+    # If the application is run as a bundle, the PyInstaller bootloader
+    # extends the sys module by a flag frozen=True and sets the app
+    # path into variable _MEIPASS'.
+    base_path = sys._MEIPASS  # noqa
+else:
+    base_path = pathlib.Path(os.path.abspath(__file__)).parent.parent
+
 config_path = os.path.join(base_path, 'config.json')
 
 config = json.load(open(config_path))
